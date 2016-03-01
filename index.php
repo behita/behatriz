aeeed462bbd31f4016ae517eda5f5a332eff3a7a
behita <behatriz.13gnicasio@gmail.com>
2016-02-29 20:36:50 -0500

    mi proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>nicasio</title>
+</head>
+<body>
+<marquee>
+	<h1>beatriz</h1>
+</marquee>
+<img src="mini.gif">>
+<img src="gif minions.gif">
+</body>
+</html>
